1e073642f51ced54ed276240a53ee145efa1fc3a
Divjot Bhogal <divjotbhogal@gmail.com>
2024-10-06 11:27:05 -0400

    Added routing

---
+++ frontend/agricast/src/pages/Data.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Data: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-green-200">
+      <h1 className="text-4xl font-bold">Data Page</h1>
+    </div>
+  );
+};
+
+export default Data;
